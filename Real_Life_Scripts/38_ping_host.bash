6383ff91d650b8e48ce4ccfae0fdc8a4a6834816
SEUTec <sje@tinet.org>
2023-09-17 17:34:51 +0200

    ping_host reading ips from files.

--- Real_Life_Scripts/38_ping_host.bash
+++ Real_Life_Scripts/38_ping_host.bash
@@ -9,7 +9,7 @@
 host_ip=192.168.0.4
 #list="1 2 3 4"         # define a list between "", white space separated
 host_ips="192.168.0.1 192.168.0.2 192.168.0.105 192.168.0.106"
-#hl_file=./hosts_list   # host_list file name
+hl_file=./hosts_list   # host_list file name with one op per line
 
 : '
 echo
@@ -22,14 +22,28 @@ else
   echo $host_ip Do Not reply.
 fi
 '
-
-# Multiple Hosts host_ips
+: '
+# Multiple Hosts from a host_ips
 echo
 #echo list: $list
 #echo host_ips: $host_ips
 #for ip in 1 2 3 4
 #for ip in $list
 for ip in $host_ips
+do
+  #echo ip: $ip
+  ping -c1 $ip &> /dev/null
+  if [[ $? = 0 ]]; then
+    echo $ip is On Line.
+  else
+    echo $ip Do Not reply.
+  fi
+done
+'
+
+# Multiple Hosts from a file
+echo
+for ip in $(cat $hl_file)
 do
   #echo ip: $ip
   ping -c1 $ip &> /dev/null
